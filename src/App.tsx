001dbc5993532df67ee84b7d990b3f788b10cc84
mcm <joohyuna2@gmail.com>
2026-04-15 15:05:48 +0900

    Home 연습

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import Home from "./routes/Home.tsx";
 import Detail from "./routes/Detail.tsx";
 
 function App() {
-
-    return <BrowserRouter>
-        <Routes>
-            <Route path={"/"} element={<Home />} />
-            <Route path={"/:id"} element={<Detail />} />
-        </Routes>
-    </BrowserRouter>
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<Home />} />
+                <Route path={"/:id"} element={<Detail />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
